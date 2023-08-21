9df5d32d65380cbe139c21c4c4b8ca56506fc47d
Kenneth Pauletti <kennethpauletti@gmail.com>
2023-08-21 11:57:29 -0400

    Remove array schemas, return values are now typed (#5)

--- index.ts
+++ index.ts
@@ -1,86 +1,47 @@
-import { z, ZodType, type ZodError } from "zod";
+import { z, ZodType, type ZodError, ZodObject } from "zod";
 export { z } from "zod";
 
+type Simplify<T> = { [P in keyof T]: T[P] } & {};
 type Schema = Record<string, ZodType>;
-type SchemaArray = Array<string>;
-
-const arrayToSchema = (keys: SchemaArray) => {
-    return keys.reduce((acc, key) => {
-        acc[key] = z.string();
-        return acc;
-    }, {} as Schema);
-};
-
-const schemaToZodObject = (schema: Schema, defaults: boolean) => {
-
-    if (defaults) {
-        return z.object(applyDefaults(schema));
-    }
-
-    return z.object(schema);
-};
 
 const onValidationError = (error: ZodError) => {
     console.error("Invalid env variables:", error.flatten().fieldErrors);
     throw new Error("Invalid env variables");
 };
 
-const throwIfEmpty = (obj: Schema | SchemaArray) => {
-    if (!obj) {
+const isValidSchema = (obj: Schema) => {
+    if(!obj) {
         throw new Error("No schema provided");
     }
-
-    const keys = Array.isArray(obj) ? obj : Object.keys(obj);
-
-    if (keys.length === 0) {
+    if(Object.keys(obj).length === 0) {
         throw new Error("Empty schema provided");
     }
-};
-
-const throwIfInvalid = (obj: Schema | SchemaArray) => {
-    throwIfEmpty(obj);
-
-    if (!isValidSchema(obj)) {
-        throw new Error("Invalid schema provided");
+    for (const key in obj) {
+        if (typeof obj[key] !== "object" || !(obj[key] instanceof ZodType)) {
+            throw new Error(`Invalid schema for key "${key}"`);
+        }
     }
-};
-
-const isValidSchema = (obj: Schema | SchemaArray) => {
-    if (Array.isArray(obj)) {
-        return obj.every((key) => typeof key === "string");
-    }
-    return Object.values(obj).every((value) => {
-        return typeof value === "object" && value instanceof ZodType;
-    });
-};
-
-const applyDefaults = (obj: Schema) => {
-    return Object.keys(obj).reduce((acc, key) => {
-        const field = obj[key] as ZodType;
-
-        acc[key] = field.default(key + "_DEFAULT_VALUE");
-
-        return acc;
-    }, {} as Schema);
 };
 
 type EnvOpts = {
     onValidationError?: (error: ZodError) => void;
-    defaults?: boolean;
 };
 
-export const getEnv = <T extends {[key: string]: string}>(schema: Schema | SchemaArray, opts?: EnvOpts) => {
-    throwIfInvalid(schema);
-
-    const _schema = Array.isArray(schema)
-        ? schemaToZodObject(arrayToSchema(schema), opts?.defaults ?? false)
-        : schemaToZodObject(schema, opts?.defaults ?? false);
+export const getEnv = <T extends Schema>(
+    schema: T,
+    opts?: EnvOpts
+): Readonly<Simplify<z.infer<ZodObject<T>>>> => {
+    
+    isValidSchema(schema);
+    
+    const _schema = z.object(schema);
     const parsed = _schema.safeParse(process.env);
+
 
     if (parsed.success === false) {
         opts?.onValidationError ? opts.onValidationError(parsed.error) : onValidationError(parsed.error);
         throw new Error("Invalid env variables");
     }
 
-    return parsed.data as T;
+    return parsed.data;
 };
